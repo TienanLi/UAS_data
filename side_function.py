4a23f00d09bceee3ca39c9493263bc79452f7cd8
TienanLi <ironmantnt@live.com>
2019-10-31 20:13:44 -0400

    modified algorithm

--- side_function.py
+++ side_function.py
@@ -1,13 +1,16 @@
 from pyproj import Proj, transform
 import shapefile
-from math import sin, cos, sqrt, atan2, radians
+from math import sin, cos, sqrt, atan2, radians, ceil
 from datetime import timedelta
-import gdal
+# import gdal
 import numpy as np
-from matplotlib.ticker import PercentFormatter
+from matplotlib.ticker import PercentFormatter,MaxNLocator
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import random
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from scipy import stats
 
 def covert_coordinate_from_4326_to_MA(point):
     y1, x1 = point[0], point[1]
@@ -45,10 +48,7 @@ def find_furthest_point(point,point_set,included_set,r):
 def cal_distance_simplified(point_1,point_2):
     return (sqrt((point_1[0]-point_2[0])**2+(point_1[1]-point_2[1])**2))
 
-
-
 def find_minimum_time_gap(start_time_set,duration_set,severity,subtype,excluded_set,original_point_set):
-
     index_set=[]
     for p in excluded_set:
         index_set.append(original_point_set.index(p))
@@ -57,7 +57,8 @@ def find_minimum_time_gap(start_time_set,duration_set,severity,subtype,excluded_
         indexed_incident[i]+=[excluded_set[i],severity[i],subtype[i]]
     indexed_incident.sort(key=lambda index_time: index_time[0])
     if len(indexed_incident)==1:
-        return 1e8,1e8,1,[],[]
+        return 1e8,1e8,1,[],[],([subtype[index_set[0]]],[duration_set[index_set[0]]])
+
 
     start_time=[t[0] for t in indexed_incident]
     end_time=[t[1] for t in indexed_incident]
@@ -65,6 +66,12 @@ def find_minimum_time_gap(start_time_set,duration_set,severity,subtype,excluded_
     severity=[t[3] for t in indexed_incident]
     subtype=[t[4] for t in indexed_incident]
 
+    SIC_duration=[duration_set[i] for i in index_set]
+    SIC_severity=severity.copy()
+
+    for i in range(len(subtype)):
+        if subtype[i] in ['MVA with injury','multi vehicle accident','MVA without injury']:
+            subtype[i]='MVA'
     same_time_threshold=25
     gap_set=[]
     min_gap = timedelta(1e8)
@@ -73,6 +80,7 @@ def find_minimum_time_gap(start_time_set,duration_set,severity,subtype,excluded_
     correlated_incident_set=[]
     parallel_incident_set=[]
 
+    MIC_set=[]
     for i in range(1,len(indexed_incident)):
         # gap
         gap=max(timedelta(0),start_time[i]-current_end_time)
@@ -89,7 +97,7 @@ def find_minimum_time_gap(start_time_set,duration_set,severity,subtype,excluded_
             correlated_incidents,parrallel_incidents=find_correlated(current_occuring)
 
             for full_info in correlated_incidents:
-
+                MIC_set.append(i)
                 c_i=full_info
                 # c_i=[f[2] for f in full_info]
                 if len(correlated_incident_set)==0:
@@ -118,7 +126,12 @@ def find_minimum_time_gap(start_time_set,duration_set,severity,subtype,excluded_
     except:
         same_time_incident = 1
 
-    return td_mins,td_average,same_time_incident,correlated_incident_set,parallel_incident_set
+    MIC_set=sorted(list(set(MIC_set)),reverse=True)
+    for MIC in MIC_set:
+        SIC_duration.pop(MIC)
+        SIC_severity.pop(MIC)
+
+    return td_mins,td_average,same_time_incident,correlated_incident_set,parallel_incident_set,(SIC_severity,SIC_duration)
 
 
 def find_correlated(current_occuring):
@@ -202,13 +215,47 @@ def severity_and_duration(severity):
 
 def circle_rank(center_set,point_set,r):
     circle_info=[]
-    max_cov=0
+    max_objective=0
     for center in center_set:
         included_set=find_included(center,point_set,[],r)
-        if len(included_set)>max_cov:
+        if len(included_set)==0:
+            continue
+        objective=len(included_set)
+        if objective>max_objective:
             circle_info=(center,included_set)
-            max_cov=len(included_set)
+            max_objective=objective
+    print(len(included_set))
     return circle_info
+
+def circle_rank_regulated(center_set,point_set,r,start_time_set,duration_set,severity,sub_type,original_point_set,alpha):
+    circle_info=[]
+    max_objective=1e8
+    for center in center_set:
+        included_set=find_included(center,point_set,[],r)
+        if len(included_set)==0:
+            continue
+        min_t_gap, avg_t_gap, num_same_time_incident, correlated_incidents, parallel_incidents, SIC_info = find_minimum_time_gap \
+            (start_time_set, duration_set, severity, sub_type, included_set, original_point_set)
+        single_station_cost=cal_single_cost(num_same_time_incident,r)
+        # single_station_cost=(1e8,1e8)
+        # objective=len(included_set)-alpha*single_station_cost[0]
+        objective=single_station_cost[0]/len(included_set)
+
+        if objective<max_objective:
+            circle_info=(center,included_set)
+            max_objective=objective
+            b=(min_t_gap, avg_t_gap, num_same_time_incident, correlated_incidents, parallel_incidents,SIC_info)
+    if len(circle_info)==0:
+        return circle_info,0,0,0,0,0,([],[])
+    return circle_info,b[0], b[1], b[2], b[3], b[4],b[5]
+
+def circle_random(center_set,point_set,r,start_time_set,duration_set,severity,sub_type,original_point_set):
+    center=random.choice(center_set)
+    included_set=find_included(center,point_set,[],r)
+    circle_info=(center,included_set)
+    min_t_gap, avg_t_gap, num_same_time_incident, correlated_incidents, parallel_incidents = find_minimum_time_gap \
+        (start_time_set, duration_set, severity, sub_type, included_set, original_point_set)
+    return circle_info,min_t_gap, avg_t_gap, num_same_time_incident, correlated_incidents, parallel_incidents
 
 
 def find_included(point, point_set, included_set,distance):
@@ -218,7 +265,6 @@ def find_included(point, point_set, included_set,distance):
             if distance_n<=distance:
                 included_set.append(p)
     return included_set
-
 
 
 def read_raster(raster_name):
@@ -264,6 +310,19 @@ def cal_cost(same_time_num,r):
     for num in same_time_num:
         cost_linear+=num*individual_cost_linear
         cost_exponential+=num*individual_cost_exponential
+
+    return (round(cost_linear,1),round(cost_exponential,1))
+
+
+def cal_single_cost(num,r):
+    r_in_mile=r / 1000 / 1.60934
+    alpha=0.5
+    b=1
+    individual_cost_linear=r_in_mile*alpha+b
+    base=1.25
+    individual_cost_exponential=base**r_in_mile
+    cost_linear=num*individual_cost_linear
+    cost_exponential=num*individual_cost_exponential
 
     return (round(cost_linear,1),round(cost_exponential,1))
 
@@ -542,6 +601,28 @@ def draw_figures(min_gap,key_min,correlated_num,parallel_num,same_time_num,r):
     plt.savefig('same time incident.png')
     plt.close()
 
+def frequency_and_MICSC(center_set_points,key_center_set,correlated_num,parallel_num):
+    y=correlated_num
+    # y=parallel_num
+
+    fig = plt.figure(figsize=(5, 5), dpi=100, tight_layout=True)
+    ax = fig.add_subplot(111)
+    frequency = [f[1] for f in center_set_points]
+    frequency_center = [c[1] for c in key_center_set]
+    correlated_key = [y[i] for i in range(len(frequency_center))]
+    plt.scatter(frequency, y, c='b', label='regular station', s=48, edgecolor='k')
+    plt.scatter(frequency_center, correlated_key, c='r', label='key station', s=48, edgecolor='k')
+    plt.legend()
+    plt.xlabel('Number of incidents', fontsize=16)
+    plt.ylabel('Number of MICs', fontsize=16)
+    r_in_mile = int(r / 1000 / 1.60934)
+    plt.title('Radius = ' + str(r_in_mile) + ' miles')
+    axes = plt.gca()
+    y_limit = axes.get_ylim()
+    y_limit = [-0.5, ceil(y_limit[1] / 5) * 5]
+    axes.set_ylim(y_limit)
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.savefig('relationship.png')
 
 def cal_distance(point_1,point_2):
     # approximate radius of earth in km
@@ -556,3 +637,108 @@ def cal_distance(point_1,point_2):
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = R * c
     return distance
+
+def linear_regression(X,Y):
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+    # Train the model using the training sets
+    regr.fit(X, Y)
+    # Make predictions using the testing set
+    y_pred = regr.predict(X)
+
+    newX = np.append(np.ones((len(X),1)), X, axis=1)
+    MSE = (sum((Y-y_pred)**2))/(len(newX)-len(newX[0]))
+    params = np.append(regr.intercept_, regr.coef_)
+
+    var_b = MSE * (np.linalg.inv(np.dot(newX.T, newX)).diagonal())
+    sd_b = np.sqrt(var_b)
+    ts_b = params / sd_b
+    p_values = [2 * (1 - stats.t.cdf(np.abs(i), (len(newX) - 1))) for i in ts_b]
+    sd_b = np.round(sd_b, 3)
+    ts_b = np.round(ts_b, 3)
+    params = np.round(params, 4)
+    print('Intercept and Coefficients: \n', params)
+    print("p value:",p_values)
+
+    return params
+
+    # Plot outputs
+    # plt.scatter(X, Y, color='black')
+    # plt.plot(X, y_pred, color='blue', linewidth=3)
+    # plt.xticks(())
+    # plt.yticks(())
+    # plt.show()
+
+def network_algorithm(center_set,point_set,start_time_set,duration_set,severity,sub_type,r,threshold,method):
+    key_threshold = r / 1000 / 1.60934 * 12
+    original_point_set = point_set.copy()
+    final_center_set = []
+    key_sum=0
+    key_count=0
+    key_center_set=[]
+    min_t_gap_set=[]
+    average_t_gap_set=[]
+    key_min_t_gap_set=[]
+    key_average_t_gap_set=[]
+    correlated_incidents_set=[]
+    parallel_incident_set=[]
+    same_time_incident_set=[]
+    MIC_full_info=[]
+    SIC_full_info=[[],[]]
+    circle_info=[]
+    flink = open('stations.txt','w+')
+
+    while len(point_set)>(len(original_point_set)*threshold):
+        # rank
+        if method in ['rank','rank_regulated']:
+            regulation_factor = 0
+            circle_info,min_t_gap, avg_t_gap, num_same_time_incident, correlated_incidents, parallel_incidents,SIC_info\
+                = circle_rank_regulated(center_set, point_set,r,start_time_set,duration_set,severity,sub_type,original_point_set.copy(),
+                              alpha=regulation_factor)
+            if len(circle_info)==0:
+                break
+        #random
+        if method=='random':
+            if len(circle_info)>0:
+                center_set.remove(circle_info[0])
+            if not len(center_set):
+                break
+            circle_info = circle_random(center_set,point_set,r,start_time_set,duration_set,severity,sub_type,original_point_set)
+            if not len(circle_info):
+                continue
+
+        excluded_set=circle_info[1]
+        final_center_set.append((circle_info[0],len(excluded_set)))
+
+        MIC_full_info+=correlated_incidents
+        SIC_full_info=[SIC_full_info[i]+SIC_info[i] for i in range(2)]
+        min_t_gap_set.append(min_t_gap)
+        average_t_gap_set.append(avg_t_gap)
+        correlated_incidents_set.append(len(correlated_incidents))
+        parallel_incident_set.append(len(parallel_incidents))
+        same_time_incident_set.append(num_same_time_incident)
+        max_cov=len(excluded_set)
+        if max_cov>=key_threshold:
+            key_center_set.append((circle_info[0], len(excluded_set)))
+            key_sum+=max_cov
+            key_count+=1
+            key_min_t_gap_set.append(min_t_gap)
+            key_average_t_gap_set.append(avg_t_gap)
+            if max_cov>=600:
+                flink.write('%s,%s,%s,%s,%s,%s\n' % (
+                circle_info[0][0], circle_info[0][1], len(circle_info[1]), min_t_gap, 'c', num_same_time_incident))
+            else:
+                flink.write('%s,%s,%s,%s,%s,%s\n' % (
+                    circle_info[0][0], circle_info[0][1], len(circle_info[1]), min_t_gap, 'k', num_same_time_incident))
+        else:
+            flink.write('%s,%s,%s,%s,%s,%s\n' % (
+                circle_info[0][0], circle_info[0][1], len(circle_info[1]), min_t_gap, 'r', num_same_time_incident))
+
+        excluded_set.sort(reverse=True)
+        for point in excluded_set:
+            point_set.remove(point)
+
+    # MIC_analyses(MIC_full_info)
+
+    return final_center_set,original_point_set,key_center_set,\
+           min_t_gap_set,key_min_t_gap_set,correlated_incidents_set,parallel_incident_set, same_time_incident_set
