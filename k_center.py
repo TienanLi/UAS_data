36415ad486d4bff82d77b3200607a281e9f17a92
TienanLi <ironmantnt@live.com>
2021-06-26 22:40:59 +0800

    directly process from the excel instead of the intermediate point pickles

--- k_center.py
+++ k_center.py
@@ -19,7 +19,13 @@ def read_point_and_process():
     new_point_set=[(p[0]+random.uniform(-1e-09,1e-09),p[1]+random.uniform(-1e-09,1e-09)) for p in point_set]
     new_point_set=[covert_coordinate_from_4326_to_MA(p) for p in new_point_set]
 
-    greedy_find(new_point_set,start_time,duration,severity,sub_type)
+    global TH,r
+    TH = 0.05 # how much point can be ignored, i.e., 0.05 means >=95% coverage
+    for r in [5, 10]:
+        print('radius:',r)
+        r = r * 1000
+        r = r * 1.60934
+        greedy_find(new_point_set,start_time,duration,severity,sub_type)
     # greedy_generate(point_set)
 
 def greedy_find(point_set,start_time_set,duration_set,severity,sub_type):
@@ -52,13 +58,6 @@ def greedy_find(point_set,start_time_set,duration_set,severity,sub_type):
     # raster_coverage(center_set_points,key_center_set,raster_area2,'Bob 1991',r)
     # raster_coverage(center_set_points,key_center_set,raster_area3,'Floyd 1999',r)
 
-
 if __name__ == '__main__':
-    global TH,r
-    TH = 0.05 # how much point can be ignored, i.e., 0.05 means >=95% coverage
-    for r in [5, 10]:
-        print('radius:',r)
-        r = r * 1000
-        r = r * 1.60934
         read_point_and_process()
 
